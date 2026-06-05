1360029c7e98fad128be6e8f5a0aabe0977fac4e
Gunya5885 <gabarr6306@stu.mylusd.org>
2026-06-05 14:48:52 -0700

    Make new changes

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-BUTTON_1 = 5   # Button 1 -> hostile
-BUTTON_2 = 6   # Button 2 -> explode only if hostile was on
-BUTTON_3 = 13  # Button 3 -> explode only if hostile was on
-BUTTON_4 = 19  # Button 4 -> explode from idle or hostile
+BUTTON_1 = 5
+BUTTON_2 = 6
+BUTTON_3 = 13
+BUTTON_4 = 19
 
 HOSTILE_LED = 27
 EXPLODE_LED = 22
 
-GPIO.setup(BUTTON_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(BUTTON_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(BUTTON_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(BUTTON_4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BUTTON_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setup(HOSTILE_LED, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(EXPLODE_LED, GPIO.OUT, initial=GPIO.LOW)
@@ -27,7 +28,7 @@ def set_state(new_state):
     GPIO.output(HOSTILE_LED, GPIO.HIGH if state == "hostile" else GPIO.LOW)
     GPIO.output(EXPLODE_LED, GPIO.HIGH if state == "explode" else GPIO.LOW)
 
-last1 = last2 = last3 = last4 = False
+last1 = last2 = last3 = last4 = True
 
 try:
     while True:
@@ -36,16 +37,16 @@ try:
         b3 = GPIO.input(BUTTON_3)
         b4 = GPIO.input(BUTTON_4)
 
-        if b1 and not last1:
+        if not b1 and last1:
             set_state("hostile")
 
-        if b2 and not last2 and state == "hostile":
+        if not b2 and last2 and state == "hostile":
             set_state("explode")
 
-        if b3 and not last3 and state == "hostile":
+        if not b3 and last3 and state == "hostile":
             set_state("explode")
 
-        if b4 and not last4 and state in ("idle", "hostile"):
+        if not b4 and last4 and state in ("idle", "hostile"):
             set_state("explode")
 
         last1, last2, last3, last4 = b1, b2, b3, b4
